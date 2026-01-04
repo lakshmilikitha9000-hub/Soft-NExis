255c7c7fa509045773234f882ba5b91aeca0c6fe
lakshmilikitha9000-hub <lakshmilikitha9000@gmail.com>
2026-01-04 19:29:58 +0530

    Add files via upload

---
+++ TASK4/sketch.ino
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include "DHT.h"
+
+#define DHTPIN 4
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+
+  if (isnan(humidity) || isnan(temperature)) {
+    Serial.println("Failed to read from DHT sensor!");
+  } else {
+    Serial.print("Humidity: "); Serial.print(humidity); Serial.print(" %  ");
+    Serial.print("Temperature: "); Serial.print(temperature); Serial.println(" °C");
+  }
+
+  delay(2000);
+}
+
